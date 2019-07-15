de0d9b7dd0883892cb4c4f4faa20e5660c514804
Asante Adarkwa Usman <asante.au96@gmail.com>
2019-07-15 11:09:30 -0400

    Folders added to repo

---
+++ myAttendanceSystem/app/src/main/java/com/example/myattendancesystem/aiti_homepage.java
@@ -0,0 +1,14 @@
+package com.example.myattendancesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aiti_homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aiti_homepage);
+    }
+}
