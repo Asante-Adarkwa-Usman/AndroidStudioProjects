eea13e7d15d45bf7dc3ccc79a10a23564f90d924
Asante-Usman <asante.au96@gmail.com>
2019-07-24 16:51:36 +0000

    ...

--- AITIKACEAPP/app/src/main/java/com/example/aiti_kaceapp/admin.java
+++ AITIKACEAPP/app/src/main/java/com/example/aiti_kaceapp/admin.java
@@ -3,8 +3,10 @@ package com.example.aiti_kaceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class admin extends AppCompatActivity {
     Button signup;
@@ -17,5 +19,19 @@ public class admin extends AppCompatActivity {
         signup = (Button)findViewById(R.id.adminGetStarted);
         staffId = (EditText) findViewById(R.id.adminStaffId);
         staffPassword = (EditText) findViewById(R.id.adminPassword);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String iden, passcode;
+                iden = staffId.getText().toString().trim();
+                passcode = staffPassword.getText().toString().trim();
+                if(iden.length() == 0){
+                    Toast.makeText(admin.this, "Staff id field cannot be empty", Toast.LENGTH_SHORT).show();
+                }else  if(passcode.length() == 0) {
+                    Toast.makeText(admin.this, "Password field cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
